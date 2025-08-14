337a6abe557ee0ba43195a9099895e6fb8910f05
imsippinmilk <jhsilva@bu.edu>
2025-08-14 01:51:25 -0400

    feat: add light and dark theme toggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
+import './theme.css';
 
 // The entry point for the Deck Arcade application. It mounts the
 // root React component into the DOM. We use React's strict mode
@@ -8,5 +9,5 @@ import App from './app/App';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
